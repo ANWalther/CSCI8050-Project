c97b66ea5a0b297c18719f0dabb84e239a0dbc36
Alexis <60196814+ANWalther@users.noreply.github.com>
2025-04-30 12:15:33 -0500

    outline initial analysis functions

--- graphs/AnalyzeGraphs.py
+++ graphs/AnalyzeGraphs.py
@@ -1,6 +1,33 @@
 import networkx as nx
 import CreateGraphs
+import datetime as dt
+import os
 
+player_origin = "Helgen"
+
+def ExportReport(filename:str, report:str):
+    with open(os.path.join(os.getcwd(), "output", filename), 'w') as report_file:
+        report_file.write(f"{dt.datetime.now()}\n")
+        report_file.write(report)
+    print(f"Report exported, check output/{filename}")
+
+def GenerateBasicReport(graph:nx.Graph) -> str:
+    report = "Basic Analytics\n\n"
+
+    # num of nodes
+    # graph density
+    # degree histogram - highest and lowest weight nodes (5 of each?)
+    # degree centrality of origin
+    # dispersion centrality of origin
+
+    return report
+
+def GenerateCommunicabilityReport(graph:nx.Graph) -> str:
+    report = "Communicability Analytics\n\n"
+
+    # communicability - write as a matrix
+
+    return report
 
 def main():
     print("Analyze Graphs Main")
