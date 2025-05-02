dbd1f5f4a75c11e5f2851bb5849f738c281a0363
Alexis <60196814+ANWalther@users.noreply.github.com>
2025-05-02 18:50:54 -0500

    working on graph visuals

--- graphs/DrawGraphs.py
+++ graphs/DrawGraphs.py
@@ -1,23 +1,35 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.image as img
 import CreateGraphs
+import QuestData
 
-graph_scale = 10.0
+hold_colors = {
+    "Eastmarch" : "#ff3300", "Falkreath Hold" : "#ff9900", "Haafingar" : "#ffff66",
+    "Hjaalmarch" : "#33cc33", "The Pale" : "#00ffff", "The Reach" : "#0066ff",
+    "The Rift" : "#9966ff", "Whiterun Hold" : "#669999", "Winterhold" : "#666699"
+}
 
-def GetLocationsPositions(locations_graph:nx.Graph):
+def GetNodeDrawData(locations_graph:nx.Graph):
     node_pos = {}
-    x_coords = nx.get_node_attributes(locations_graph, "x_coord", default=0)
-    y_coords = nx.get_node_attributes(locations_graph, "y_coord", default=0)
+    node_col = {}
+
+    x_coords = nx.get_node_attributes(locations_graph, "x_coord", default=None)
+    y_coords = nx.get_node_attributes(locations_graph, "y_coord", default=None)
+    holds = nx.get_node_attributes(locations_graph, "hold")
 
     for node in locations_graph.nodes:
+        if not node in x_coords or not node in y_coords or x_coords[node] == None or y_coords[node] == None:
+            print(node)
+            continue
         x = x_coords[node]
         y = y_coords[node]
         node_pos[node] = (x,y)
-        if x == y and x == 0:
-            print(node)
+        node_col[node] = hold_colors[holds[node]]
+        
     
     #node_pos = nx.rescale_layout_dict(node_pos, graph_scale)    
-    return node_pos
+    return node_pos, node_col
     
 
 def DrawQuestlinesGraph(questlines_graph:nx.DiGraph):
@@ -28,8 +40,14 @@ def DrawQuestlinesGraph(questlines_graph:nx.DiGraph):
     return
 
 def DrawLocationsGraph(locations_graph:nx.Graph):
-    # draw edges then node and edge labels for more fine-tuned control
-    nx.draw_networkx(locations_graph, pos=GetLocationsPositions(locations_graph))
+    # draw edges then nodes and edge labels for more fine-tuned control
+    #nx.draw_networkx(locations_graph, pos=GetLocationsPositions(locations_graph))
+
+    node_pos, node_col = GetNodeDrawData(locations_graph)
+    overworld_subgraph = nx.induced_subgraph(locations_graph, node_pos.keys())
+    #nx.draw_networkx_edges(overworld_subgraph, pos=node_pos, node_size=10)
+    nx.draw_networkx_nodes(overworld_subgraph, pos=node_pos, node_color=node_col.values(), node_size=100)
+    nx.draw_networkx_labels(overworld_subgraph, pos=node_pos, font_size=10)
     plt.show()
     return
 
@@ -41,15 +59,10 @@ def main():
     
     print("Draw Graphs Main")
 
-    # Test drawing questlines graph
-    #print("Draw Questlines Graph")
-    #questData = CreateGraphs.ReadQuests()
-    #questlines_graph = CreateGraphs.CreateQuestlinesGraph(questData)
+    quest_data = QuestData.ReadQuests()
+    locations_graph = CreateGraphs.ReadLocations(False)
+    CreateGraphs.UpdateQuestLocationsGraph(quest_data, locations_graph)
 
-    # DrawQuestlinesGraph(questlines_graph)
-
-    # Test drawing locations graph
-    locations_graph = CreateGraphs.ReadLocations(True)
     DrawLocationsGraph(locations_graph)
 
     # Test drawing locations graph with quest edges
